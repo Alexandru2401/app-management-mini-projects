d3f92f4674ba3b7cc849ea3bb5ee06b820d5cbf9
Alexandru2401 <a.toma231@yahoo.com>
2024-08-09 10:32:12 +0300

    adding a new button components for more flexibility and NoProjectSelected components, which will be the component rendered on the screen if no project was selected.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Sidebar from "./components/SideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
