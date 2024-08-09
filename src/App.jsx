5c632d564e5882093e7bad08b9380275ced0bf7b
Alexandru2401 <a.toma231@yahoo.com>
2024-08-09 11:27:02 +0300

    adding state in app components for manage state beetwen NewProject component and NoProjectSelected components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import Sidebar from "./components/SideBar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectsState((prevProjects) => {
+      return {
+        ...prevProjects,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleAddProject} />
+      {content}
     </main>
   );
 }
